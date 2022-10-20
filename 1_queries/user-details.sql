10fe7f64c2b337b921fb98fd7dd7f44b534db541
Rosanna Zhang <rosannazhang92@gmail.com>
2022-10-19 19:19:08 -0700

    Queries

---
+++ 1_queries/user-details.sql
@@ -0,0 +1,8 @@
+-- details on single user 
+-- Select their id, name, email, and password.
+-- Select a single user using their email address. 
+-- Use tristanjacobs@gmail.com for now.
+
+SELECT id, name, email, password
+FROM users
+WHERE email = 'tristanjacobs@gmail.com';
