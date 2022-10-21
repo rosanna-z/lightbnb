2a8cc25eba59f34b974cc5a5e56ecb57c5e7ac1a
Rosanna Zhang <rosannazhang92@gmail.com>
2022-10-20 21:33:19 -0700

    Update function using SQL query to allow user to add listing

--- LightBnB_WebApp/server/database.js
+++ LightBnB_WebApp/server/database.js
@@ -234,7 +234,7 @@ const addProperty = function(property) {
     post_code) 
   VALUES (
   $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
-
+    RETURNING *;
           `, [property.title, 
             property.description, 
             property.owner_id, 
